fbc43c1e2f43ae1c75db415ba94a659df7d737a2
Matthew Li <matthew.li10@education.nsw.gov.au>
2023-06-03 13:15:52 +1000

    add font, update styles, app, index, config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
+  content: [
+    './index.html',
+    './src/**/*.{tsx,ts}'
+  ],
   plugins: [],
+  theme  : { extend: { fontFamily: { sans: 'MartianMono' } } }
 }
-
